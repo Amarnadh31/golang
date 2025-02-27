e6eecd54f760c83247398c7ba1f3e89ecfa413c7
Amar <cberg2386@gmail.com>
2025-02-27 09:04:57 +0530

    golang codes

--- loops/loops.go
+++ loops/loops.go
@@ -2,10 +2,8 @@
 package loops
 
 import (
-
 	"fmt"
 )
-
 
 func LoopsFunc( ){
 
@@ -13,47 +11,42 @@ func LoopsFunc( ){
 	m := "Left"
 	n := "Right"
 
-			for {
-				fmt.Println("Enter the start number: ")
-				_, err1 := fmt.Scan(&start)
-				if err1 != nil {
-					fmt.Println("Please enter vaild input(0-9)")
+	for {
+		fmt.Println("Enter the start number: ")
+		_, err1 := fmt.Scan(&start)
+		if err1 != nil {
+			fmt.Println("Please enter vaild input(0-9)")
 
-					var discord string
-					fmt.Scanln(&discord)
-					continue	
-				} 
-				break
-			}
-			
-			for{
-				fmt.Println("Enter the end number: ")
-				_,err2 := fmt.Scan(&end)
-				if err2 != nil {
-					fmt.Println("Please enter vaild input(0-9)")
+			var discord string
+			fmt.Scanln(&discord)
+			continue	
+		} 
+		break
+	}
+	
+	for{
+		fmt.Println("Enter the end number: ")
+		_,err2 := fmt.Scan(&end)
+		if err2 != nil {
+			fmt.Println("Please enter vaild input(0-9)")
 
-					var discord string
-					fmt.Scanln(&discord)
-					continue
-				}
-				break
+			var discord string
+			fmt.Scanln(&discord)
+			continue
+		}
+		break
+	}
 
-			}
+	for i:= start; i< end; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println(m+n)
+		}else if i%5 == 0 {
+			fmt.Println(n)
+		}else if i%3 == 0{
+			fmt.Println(m)
+		}else {
+			fmt.Println(i)
+		}
 
-
-			for i:= start; i< end; i++ {
-				if i%3 == 0 && i%5 == 0 {
-					fmt.Println(m+n)
-				}else if i%5 == 0 {
-					fmt.Println(n)
-				}else if i%3 == 0{
-					fmt.Println(m)
-				}else {
-					fmt.Println(i)
-				}
-
-			}
-
-
-
+	}
 }
